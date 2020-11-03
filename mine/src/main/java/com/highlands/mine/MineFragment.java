a1b9aa1c6a3a78d42a2b2fdb717923a5e962e76e
徐亮亮 <xu.liangliang1992@qq.com>
2020-11-03 21:08:07 +0800

    MineFragment

--- mine/src/main/java/com/highlands/mine/MineFragment.java
+++ mine/src/main/java/com/highlands/mine/MineFragment.java
@@ -8,9 +8,12 @@ import com.alibaba.android.arouter.launcher.ARouter;
 import com.highlands.common.base.fragment.BaseLazyFragment;
 import com.highlands.common.constant.RouterUrl;
 import com.highlands.common.util.ShapeUtil;
+import com.highlands.common.util.SystemUtil;
 import com.highlands.mine.databinding.MineFragmentBinding;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import timber.log.Timber;
 
@@ -42,22 +45,42 @@ public class MineFragment extends BaseLazyFragment<MinePresenter> implements Min
         binding.setViewModel(mViewModel);
         binding.setLifecycleOwner(this);
 
-        ShapeUtil.setShapeColor(binding.clCollect,mActivity,R.color.white_EFF4FF);
-        ShapeUtil.setShapeColor(binding.clFocus,mActivity,R.color.yellow_FFF9F0);
-        ShapeUtil.setShapeColor(binding.clComment,mActivity,R.color.green_F2FDFF);
-        ShapeUtil.setShapeColor(binding.clSupport,mActivity,R.color.yellow_FFF7F4);
+        ShapeUtil.setShapeColor(binding.clCollect, mActivity, R.color.white_EFF4FF);
+        ShapeUtil.setShapeColor(binding.clFocus, mActivity, R.color.yellow_FFF9F0);
+        ShapeUtil.setShapeColor(binding.clComment, mActivity, R.color.green_F2FDFF);
+        ShapeUtil.setShapeColor(binding.clSupport, mActivity, R.color.yellow_FFF7F4);
+        ShapeUtil.setShapeColor(binding.clVip, mActivity, R.color.yellow_FFDB9F);
     }
 
     @Override
     public void initListener() {
-        Timber.tag(TAG).d("onViewCreated initListener");
+        final Observer<Boolean> paidObserver = isPaid -> {
+            ConstraintLayout.LayoutParams lp = (ConstraintLayout.LayoutParams) binding.clMiddle.getLayoutParams();
+            lp.topMargin = SystemUtil.dip2px(mActivity, isPaid ? 59 : 14);
+            binding.clMiddle.setLayoutParams(lp);
+        };
+        mViewModel.isPaid().observe(this, paidObserver);
+
         binding.tvLogin.setOnClickListener(this);
         binding.tvRegister.setOnClickListener(this);
+        binding.tvRenew.setOnClickListener(this);
+        binding.tvChat.setOnClickListener(this);
+        binding.llChat.setOnClickListener(this);
+        binding.tvHistory.setOnClickListener(this);
+        binding.tvMail.setOnClickListener(this);
+        binding.tvCusService.setOnClickListener(this);
+        binding.llCustomService.setOnClickListener(this);
+        binding.clCollect.setOnClickListener(this);
+        binding.clFocus.setOnClickListener(this);
+        binding.clComment.setOnClickListener(this);
+        binding.clSupport.setOnClickListener(this);
+        binding.tvLogout.setOnClickListener(this);
     }
 
     @Override
     public void initData() {
         Timber.tag(TAG).d("onActivityCreated");
+        mViewModel.isPaid().setValue(true);
     }
 
     @Override
@@ -72,10 +95,30 @@ public class MineFragment extends BaseLazyFragment<MinePresenter> implements Min
 
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.tv_login){
+        if (v.getId() == R.id.tv_login) {
             ARouter.getInstance().build(RouterUrl.TAX_LOGIN).navigation();
-        }else if(v.getId()==R.id.tv_register){
+        } else if (v.getId() == R.id.tv_register) {
             ARouter.getInstance().build(RouterUrl.TAX_REGISTER).navigation();
+        } else if (v.getId() == R.id.tv_renew) {
+            mViewModel.isPaid().setValue(false);
+        } else if (v.getId() == R.id.tv_chat || v.getId() == R.id.ll_chat) {
+            mViewModel.isPaid().setValue(true);
+        } else if (v.getId() == R.id.tv_mail) {
+
+        } else if (v.getId() == R.id.tv_cus_service || v.getId() == R.id.ll_custom_service) {
+
+        } else if (v.getId() == R.id.tv_logout) {
+
+        } else if (v.getId() == R.id.tv_history) {
+
+        } else if (v.getId() == R.id.cl_collect) {
+
+        } else if (v.getId() == R.id.cl_focus) {
+
+        } else if (v.getId() == R.id.cl_comment) {
+
+        } else if (v.getId() == R.id.cl_support) {
+
         }
     }
 }
--- mine/src/main/java/com/highlands/mine/MineViewModel.java
+++ mine/src/main/java/com/highlands/mine/MineViewModel.java
@@ -7,6 +7,7 @@ public class MineViewModel extends ViewModel {
     // TODO: Implement the ViewModel
     private MutableLiveData<String> currentName;
     private MutableLiveData<Boolean> loggedIn;
+    private MutableLiveData<Boolean> paid;
 
     public MutableLiveData<String> getCurrentName() {
         if (currentName == null) {
@@ -14,10 +15,18 @@ public class MineViewModel extends ViewModel {
         }
         return currentName;
     }
+
     public MutableLiveData<Boolean> isLoggedIn() {
         if (loggedIn == null) {
             loggedIn = new MutableLiveData<Boolean>();
         }
         return loggedIn;
+    }
+
+    public MutableLiveData<Boolean> isPaid() {
+        if (paid == null) {
+            paid = new MutableLiveData<Boolean>();
+        }
+        return paid;
     }
 }
